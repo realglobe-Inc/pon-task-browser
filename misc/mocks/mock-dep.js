b5526a9f27a99520e2d4b81f59b9c89727ac6315
Taka Okunishi <okunishinish@gmail.com>
2017-05-25 12:22:53 +0900

    envifyでprocess.env.NODE_ENVをcompile時に上書きできるようにした

--- misc/mocks/mock-dep.js
+++ misc/mocks/mock-dep.js
@@ -1,3 +1,7 @@
 'use strict'
 
 exports.foo = () => 'This is foo!'
+
+if (process.env.NODE_ENV === 'development') {
+  console.log('Yes, this is development')
+}
---
+++ test/transforms_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for transforms.
+ * Runs with mocha.
+ */
+'use strict'
+
+const transforms = require('../lib/transforms.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('transforms', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Transforms', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
