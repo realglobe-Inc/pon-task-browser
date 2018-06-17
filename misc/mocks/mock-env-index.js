2a0f8229ed686066335b2b646d9341ada2fb5f48
okunishinishi <okunishinishi@gmail.com>
2018-06-17 22:01:35 +0900

    Support webpack

--- misc/mocks/mock-env-index.js
+++ misc/mocks/mock-env-index.js
@@ -2,8 +2,14 @@
 
 console.log('!!!hoge')
 
+function hoge () {
+  return process.env.NODE_ENV
+}
+
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('fs')
 } else {
 
 }
+
+console.log('!!is hoge ', hoge)
